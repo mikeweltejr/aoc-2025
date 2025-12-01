1db9a9c4456135f8d3d043b44f6d2da971572728
Michael Welte <m.welte@elsevier.com>
2025-12-01 12:10:27 -0500

    Small cleanup for Day1

--- day1/main.go
+++ day1/main.go
@@ -32,9 +32,10 @@ func turnDials() (int, int) {
 	}
 
 	zero_count := 0
-	zero_total_count := 0
+	zero_passes := 0
 	cur_dial := 50
 	is_zero := false
+
 	for _, dial := range dials {
 		direction := dial[0]
 		num, err := strconv.Atoi(dial[1:])
@@ -44,7 +45,6 @@ func turnDials() (int, int) {
 		}
 
 		wraps := num / 100
-		zero_total_count += wraps
 		moves := num - (wraps * 100)
 
 		switch direction {
@@ -54,8 +54,9 @@ func turnDials() (int, int) {
 			cur_dial = cur_dial + moves
 		}
 
+		zero_passes += wraps
 		if (cur_dial < 0 || cur_dial > 100) && !is_zero {
-			zero_total_count++
+			zero_passes++
 		}
 
 		cur_dial = ((cur_dial % 100) + 100) % 100
@@ -64,16 +65,16 @@ func turnDials() (int, int) {
 
 		if cur_dial == 0 {
 			zero_count++
-			zero_total_count++
+			zero_passes++
 		}
 	}
 
-	return zero_count, zero_total_count
+	return zero_count, zero_passes
 }
 
 func main() {
 	zero_count, zero_total_count := turnDials()
 
 	fmt.Printf("Zero Count: %s \n", strconv.Itoa(zero_count))
-	fmt.Printf("Zero Total Count: %s \n", strconv.Itoa(zero_total_count))
+	fmt.Printf("Zero Passes: %s \n", strconv.Itoa(zero_total_count))
 }
