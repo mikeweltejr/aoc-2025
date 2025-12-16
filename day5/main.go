ae74ff052d8b56cf840a0ce6ce204385082c1e3a
Michael Welte <mikewelte22@gmail.com>
2025-12-16 10:48:22 -0500

    Day 5 part 1

---
+++ day5/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Range struct {
+	Start int64
+	End   int64
+}
+
+func readFileInput(filename string) ([]string, []string, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Print("Error reading file")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	isAvailableIngredients := false
+	var freshIngredientRanges []string
+	var availableIngredients []string
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			isAvailableIngredients = true
+			continue
+		}
+		if !isAvailableIngredients {
+			freshIngredientRanges = append(freshIngredientRanges, scanner.Text())
+		} else {
+			availableIngredients = append(availableIngredients, scanner.Text())
+		}
+
+	}
+	return freshIngredientRanges, availableIngredients, scanner.Err()
+}
+
+func parseRanges(freshIngredients []string) []Range {
+	ranges := make([]Range, 0, len(freshIngredients))
+	for _, ingredientRange := range freshIngredients {
+		parts := strings.Split(strings.TrimSpace(ingredientRange), "-")
+		if len(parts) != 2 {
+			fmt.Println("Bad file input")
+			return nil
+		}
+
+		start, _ := strconv.ParseInt(parts[0], 10, 64)
+		end, _ := strconv.ParseInt(parts[1], 10, 64)
+		if start > end {
+			start, end = end, start
+		}
+		ranges = append(ranges, Range{Start: start, End: end})
+	}
+
+	return ranges
+}
+
+func inAnyRange(x int64, ranges []Range) bool {
+	for _, r := range ranges {
+		if x >= r.Start && x <= r.End {
+			return true
+		}
+	}
+	return false
+}
+
+func calculateAvailableFreshIngredients() {
+	freshIngredientRanges, availableIngredients, _ := readFileInput("input.txt")
+	ranges := parseRanges(freshIngredientRanges)
+
+	sum := 0
+	for _, ingredient := range availableIngredients {
+		i, _ := strconv.ParseInt(ingredient, 10, 64)
+
+		if inAnyRange(i, ranges) {
+			sum++
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+func main() {
+	calculateAvailableFreshIngredients()
+}
